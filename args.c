9c4910e1504cd968bef8118d6bb843ac27fff519
M B <85039141+m-bartlett@users.noreply.github.com>
2025-10-01 01:44:13 -0600

    Overlay and supplemental mount implementation MVP (#2)
    * consolidate resuable utility functions
    
    * setup initial arg parsing and mount+chroot testing
    
    * refactor args to ones actually used
    
    * change some arg attributes to be strings/ified
    
    * don't built test-files!
    
    * initial mounts declaration/order and setup

--- args.c
+++ args.c
@@ -1,23 +1,21 @@
 #include <stdbool.h>
-#include <stdio.h>
 #include <getopt.h>
 #include <stdlib.h>
 #include "args.h"
-
-#define eprintf(format, ...) fprintf(stderr, format __VA_OPT__(,) __VA_ARGS__)
+#include "util.h"
 
 
 #define OPSTRING_SEP_0
 #define OPSTRING_SEP_1 ':',
 #define OPSTRING_SEP_2 OPSTRING_SEP_1
-#define OPTSTRING_ITEM(short, long, has_arg, type, arg_name, help) short, OPSTRING_SEP_##has_arg
+#define OPTSTRING_ITEM(short, long, var, has_arg, type, arg, help) short, OPSTRING_SEP_##has_arg
 const char optstring[] = { ARGUMENT_LIST(OPTSTRING_ITEM) 'h', '\0' };
 #undef OPTSTRING_ITEM
 #undef OPSTRING_SEP_0
 #undef OPSTRING_SEP_1
 #undef OPSTRING_SEP_2
 
-#define LONGOPT_ITEM(short, long, has_arg, type, arg_name, help) { #long, has_arg, NULL, short },
+#define LONGOPT_ITEM(short, long, var, has_arg, type, arg, help) { long, has_arg, NULL, short },
 static const struct option long_options[] = {
     ARGUMENT_LIST(LONGOPT_ITEM)
     {"help", 0, NULL , 'h' },
@@ -26,15 +24,17 @@ static const struct option long_options[] = {
 #undef LONGOPT_ITEM
 
 
-#define HELP_ITEM(short, long, has_arg, type, arg_name, help) \
-    eprintf("  -%c, --%-15s %s\n", short, #long " " #arg_name, help);
+#define HELP_SPACE "18"
+#define HELP_ITEM(short, long, var, has_arg, type, arg, help) \
+    eprintf("  -%c, --%-"HELP_SPACE"s %s\n", short, long " " #arg, help);
 static inline void print_help(const char *prog) {
     eprintf("Usage: %s\n\n", prog);
     eprintf("Arguments:\n");
-    eprintf("  -%c, --%-15s %s\n", 'h', "help", "Show this help message and exit.");
+    eprintf("  -%c, --%-"HELP_SPACE"s %s\n", 'h', "help", "Show this help message and exit.");
     ARGUMENT_LIST(HELP_ITEM)
 }
 #undef HELP_ITEM
+#undef HELP_SPACE
 
 void parse_args(int argc, char *argv[], Arguments *arguments) {
     int option_index = 0;
@@ -49,8 +49,8 @@ void parse_args(int argc, char *argv[], Arguments *arguments) {
             #define OPTARG_0 true
             #define OPTARG_1 optarg
             #define OPTARG_2 OPTARG_1
-            #define PARSE_CASE(short, long, has_arg, type, arg_name, help) \
-                case short : arguments->long = OPTARG_##has_arg; break;
+            #define PARSE_CASE(short, long, var, has_arg, type, arg, help) \
+                case short : arguments->var = OPTARG_##has_arg; break;
                 ARGUMENT_LIST(PARSE_CASE)
             #undef OPTARG_0
             #undef OPTARG_1
@@ -59,8 +59,7 @@ void parse_args(int argc, char *argv[], Arguments *arguments) {
 
             default:
             case '?':
-                fprintf(stderr, "Try '%s --help' for more information.\n", argv[0]);
-                exit(EXIT_FAILURE);
+                fail("Try '%s --help' for more information.\n", argv[0]);
                 break;
         }
     }
---
+++ mount.h
@@ -0,0 +1,3 @@
+void create_sandbox_mounts(const char* mount_name,
+                           const char* mount_base_path,
+                           const char* source_path);
